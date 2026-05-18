2a0d47194380083e889d7a8bbf491cb4675c2b8d
huashnso <41218257+huashnso@users.noreply.github.com>
2026-05-18 15:39:06 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -123,7 +123,7 @@ const config = {
 
   //项目卡片 其中 字段"show"控制初始卡片的text是否展开
   projectcards:[ 
-    {go:"🚀 前往",img:"/img/sunshine.jpg",title:"HUAS HNSO传送门",subtitle:"1,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://huashnso.github.io/",show:false},
+    {go:"🚀 前往",img:"/img/sunshine.jpg",title:"HUAS HNSO传送门",subtitle:"一起去看世界吧", text:"世界很大充满爱且无处不在",url:"https://huashnso.github.io/",show:false},
     {go:"🗂️ 前往",img:"/img/sunshine.jpg",title:"Project 2",subtitle:"2,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://leleo.top",show:false},
     {go:"📝 前往",img:"/img/sunshine.jpg",title:"Project 3",subtitle:"3,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://leleo.top",show:false},
     {go:"👍 前往",img:"/img/sunshine.jpg",title:"Project 4",subtitle:"4,000 miles of wonder", text:"If you see this line, I've managed to get your attention.",url:"https://leleo.top",show:false},
